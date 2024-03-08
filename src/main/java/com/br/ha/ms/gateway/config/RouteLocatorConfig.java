6d973b38cdf9361178b21924009ed4707ba0926c
Roberto Rosa <roberto.rosa7@gmail.com>
2024-03-08 09:31:03 -0300

    fix: ajuste no service de gateway - fazendo a padronização e implementando spring cloud gateway standard

---
+++ src/main/java/com/br/ha/ms/gateway/config/RouteLocatorConfig.java
@@ -0,0 +1,27 @@
+package com.br.ha.ms.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+  @Value("${base.service.uri}")
+  private String uri;
+
+  @Value("${base.service.name}")
+  private String serviceName;
+
+  @Value("${base.service.context-path}")
+  private String contextPath;
+
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route(serviceName, r -> r.path(contextPath).uri(uri))
+        .build();
+  }
+}
--- src/main/java/com/br/ha/ms/service/base/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.ha.ms.service.base.repository;
-
-import com.br.ha.ms.service.base.model.UserEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository implements JpaRepository<UserEntity, Long>{
-        }
