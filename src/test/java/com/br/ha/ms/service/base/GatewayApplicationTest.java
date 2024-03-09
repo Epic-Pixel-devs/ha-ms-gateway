6a3b1e6f469c9fefde5358c9ac6d67ee7ac3aef9
Roberto Rosa <roberto.rosa7@gmail.com>
2024-03-09 00:48:26 -0300

    fix: ajuste no gateway para apontar para o serviço

--- src/test/java/com/br/ha/ms/service/base/GatewayApplicationTest.java
+++ src/test/java/com/br/ha/ms/service/base/GatewayApplicationTest.java
@@ -1,13 +1,8 @@
 package com.br.ha.ms.service.base;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EpixelApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class GatewayApplicationTest {
 
 }
